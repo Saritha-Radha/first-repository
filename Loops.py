49c1dd3d9522854449a86606415b714f9cb71573
Saritha Radha <saritha.s.radha@gmail.com>
2023-04-05 13:33:59 -0400

    First commit

---
+++ Loops.py
@@ -0,0 +1,16 @@
+x=0
+while x<=5:
+    print("hello")
+    x+=1
+
+for i in range(5):
+    for j in range(5):
+        print("i value is"+str(i)+"j value is"+str(j))
+
+for word in ['hello','hi']:
+    for letter in word:
+        print(letter)
+
+#for num in [23,3,56]:
+  #  for j in num:
+  #       print(j)   integer is not iterable
